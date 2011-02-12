2b3c21447596306d8b15be6884d481f47a7f7cb5
Dustin Rodrigues <dust.rod@gmail.com>
2011-02-12 02:26:08 -0800

    going to sleep, almost there for basic functionality

--- power.py
+++ power.py
@@ -7,6 +7,11 @@
 
 from itertools import combinations
 
+# TODO: finish diamondFree
+# TODO: finish interDict
+# TODO: finish maxFree
+
+
 # for readability, change class name from ps to PowerSet or Power or
 # something
 class ps:
@@ -28,9 +33,37 @@ class ps:
                 return False
         return True
 
-    def hasDiamond(self, eltList):
-        """NOT YET IMPLEMENTED: returns True if the list contains a 'diamond' as we have discussed in our meetings"""
-        self.makeDict(eltList)
+    def diamondFree(self, eltList):
+        """NOT FULLY IMPLEMENTED: returns True if the list is diamond-free"""
+        eltDict = self.makeDict(eltList)
+
+        for elt in eltList:
+            bot = elt
+            mids = []
+            modList = eltList[:]
+            modList.remove(elt)
+            temp = combinations(modList, 2)
+
+            # removes items that have one mid candidate that is lower
+            # than bottom
+            for item in temp:
+                if (len(item[0]) > len(bot)) and (len(item[1]) > len(bot)):
+                    mids.append(item)
+            print mids
+
+            for i in mids:
+                modModList = modList[:]
+                print modModList
+                print i[0]
+                print i[1]
+                modModList.remove(i[0])
+                modModList.remove(i[1])
+
+                if self.interDict(i[0], i[1], modModList):
+                    return True
+
+        return False
+
 
     def makeDict(self, eltList):
         """returns a list of the elements in eltList indexed by their length"""
@@ -42,10 +75,19 @@ class ps:
                 eltDict[len(elt)] = [elt]
         return eltDict
 
+    def makeList(self, eltDict):
+        """takes a dict and returns the list of elts in it"""
+        res = []
+        for i in eltDict.itervalues():
+            for j in i:
+                res.append(j)
+        return res
+
     def interDict(self, elt1, elt2, eltDict):
         """NOT YET IMPLEMENTED: returns True if the images of elt1 and elt2 have a non-trivial intersection in eltDict"""
-        im1 = self.makeDict(self.imageDict(elt1, eltDict))
-        im2 = self.makeDict(self.imageDict(elt2, eltDict))
+        pass
+        im1 = self.makeDict(self.imageList(elt1, eltDict))
+        im2 = self.makeDict(self.imageList(elt2, eltDict))
 
 
     def imageList(self, elt, elts):
@@ -81,6 +123,17 @@ class ps:
         """NOT IMPLEMENTED: returns the subposet indued between el1 and elt2 (with elt1 < elt2)"""
         pass
 
+
+    def maxFree(self, eltList):
+        """returns the size of the largest subset of eltList that does not contain a diamond"""
+        # start to look at combinations of eltList of size 1 then
+        # checking if this subset is diamond free.  if one of them is, increase
+        # the size of combinations and check again
+
+
 if __name__ == '__main__':
-    pass
+    a = ps(4)
+    a.diamondFree(a.PowerSet)
+    b = a.makeDict(a.imageList([1],a.PowerSet))
+
 
